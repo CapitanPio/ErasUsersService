c182397416fadda29a52bede67c7dabf4e1a0fc9
Claudio Espinoza Marinao <cespinoza.marina@gmail.com>
2026-03-07 14:53:53 -0300

    a

--- users_service/src/main/java/com/auth/users_service/config/CorsProperties.java
+++ users_service/src/main/java/com/auth/users_service/config/CorsProperties.java
@@ -3,9 +3,11 @@ package com.auth.users_service.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @ConfigurationProperties(prefix = "cors")
 @lombok.Data
 public class CorsProperties {
-    private String allowedOrigin;
+    private List<String> allowedOrigins;
 }
